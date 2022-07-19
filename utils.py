50bb918efd12508e9b2682d18289d17701f04122
G3om <luca.romanini@outlook.com>
2022-07-18 21:30:30 -0400

    Add function get_datasets() that is more flexible for all dataset and class_mode

--- utils.py
+++ utils.py
@@ -4,6 +4,163 @@ import torch
 from dataset import KenaukDataset, KenaukDataset_rasterio, estrie_rasterio, estrie_stack, KenaukDataset_stack, KenaukDataset_stack2, estrie_stack2
 from torch.utils.data import DataLoader, random_split
 
+# Define all paths
+# Paths (linux)
+# Paths estrie
+e_img_dir = "/mnt/Data/00_Donnees/01_trainings/02_mh_double_stack/estrie2/sen2"
+e_mask_bin_dir = "/mnt/Data/00_Donnees/01_trainings/02_mh_double_stack/estrie2/mask_bin"
+e_mask_multi_dir = "/mnt/Data/00_Donnees/01_trainings/estrie_mask/multi_label"
+e_lidar_dir = "/mnt/Data/00_Donnees/01_trainings/02_mh_double_stack/estrie2/lidar_mnt"
+
+# path kenauk
+k_img_dir= "/mnt/Data/00_Donnees/01_trainings/mh_sentinel_2/sen2_print/train"
+k_mask_dir = "/mnt/Data/00_Donnees/01_trainings/mh_sentinel_2/mask_bin/train"
+k_lidar_dir = "/mnt/Data/00_Donnees/01_trainings/mh_sentinel_2/lidar_mnt/train"
+
+k_val_img_dir = "/mnt/Data/00_Donnees/01_trainings/mh_sentinel_2/sen2_print/val"
+k_val_mask_dir = "/mnt/Data/00_Donnees/01_trainings/mh_sentinel_2/mask_bin/val"
+k_val_mnt_dir = "/mnt/Data/00_Donnees/01_trainings/mh_sentinel_2/lidar_mnt/val"
+
+# Path Kenauk Full (test)
+k_test_img = "/mnt/Data/00_Donnees/01_trainings/03_kenauk_test_full/sen2_print"
+k_test_mask = "/mnt/Data/00_Donnees/01_trainings/03_kenauk_test_full/mask_bin"
+k_test_lid = "/mnt/Data/00_Donnees/01_trainings/03_kenauk_test_full/lidar_mnt"
+
+def get_datasets(
+    train_region,
+    test_region,
+    classif_mode,
+    batch_size,
+    #train_transform,
+    #val_transform,
+    num_workers=1,
+    pin_memory=True,
+):
+
+# Defining training paths by region
+    if train_region == "kenauk":
+        train_dir = k_img_dir
+        train_mnt = k_lidar_dir
+        val_dir = k_val_img_dir 
+        val_mnt = k_val_mnt_dir
+
+        # Choosing mask paths by classif mode
+        if classif_mode == "bin":
+            print("Using training paths from Kenauk for a binary classification")
+            train_maskdir = k_mask_dir
+            val_maskdir = k_val_mask_dir
+
+        elif classif_mode == "multiclass":
+            print("This option is not available at the moment")
+        else:
+            print("Something is wrong with your Kenauk paths")
+
+        # Initiating dataset for Kenauk
+        train_ds = KenaukDataset_rasterio(
+        image_dir=train_dir,
+        mask_dir=train_maskdir,
+        mnt_dir=train_mnt,
+        #transform=train_transform
+        )
+
+        val_ds = KenaukDataset_rasterio(
+        image_dir=val_dir,
+        mask_dir=val_maskdir,
+        mnt_dir=val_mnt,
+        #transform=train_transform
+        )
+
+        test_ds = KenaukDataset_rasterio(
+        image_dir=k_test_img,
+        mask_dir=k_test_mask,
+        mnt_dir=k_test_lid,
+        #transform=train_transform
+        )
+
+    elif train_region == "estrie":
+        train_dir = e_img_dir 
+        train_mnt = e_lidar_dir
+
+        # Choosing mask paths by classif mode
+        if classif_mode == "bin":
+            print("Using training paths from Estrie for a binary classification")
+            train_maskdir = e_mask_bin_dir
+
+            # Initiate dataset
+            train_ds = estrie_stack2(
+                image_dir=train_dir,
+                mask_dir=train_maskdir,
+                mnt_dir=train_mnt,
+                #transform=train_transform
+            )
+
+        elif classif_mode == "multiclass":
+            print("Using training paths from Estrie for a multi-class classification")
+            train_maskdir = e_mask_multi_dir
+
+            # Initiate dataset
+            train_ds = estrie_rasterio(
+                image_dir=train_dir,
+                mask_dir=train_maskdir,
+                mnt_dir=train_mnt,
+                classif_mode = classif_mode
+                #transform=train_transform
+            )
+
+        # Creating train, val, test datasets
+        if test_region == 'local_split':
+            train_set_size = int(len(train_ds) * 0.90)
+            valid_set_size = (len(train_ds) - train_set_size) // 2
+            test_set_size =  len(train_ds) - (train_set_size + valid_set_size)
+            train_ds, val_ds, test_ds = random_split(train_ds, [train_set_size, valid_set_size, test_set_size])
+        elif test_region == 'kenauk_full':
+            # Initiate kenauk full dataset
+            print("**ATTENTION** There is no multi label set for full kenauk yet")
+            test_kenauk_full_ds = KenaukDataset_stack2(
+            image_dir=k_test_img,
+            mask_dir=k_test_mask,
+            mnt_dir=k_test_lid,
+            #transform=train_transform
+            )
+            train_set_size = int(len(train_ds) * 0.90)
+            valid_set_size = (len(train_ds) - train_set_size)
+            train_ds, val_ds = random_split(train_ds, [train_set_size, valid_set_size])
+            test_ds = test_kenauk_full_ds
+
+        else:
+            print("Something is wrong with your Estrie paths")
+    
+    else:
+        print("Something is wrong with your overall paths")
+
+
+    train_loader = DataLoader(
+        train_ds,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        pin_memory=pin_memory,
+        shuffle=True,
+    )
+
+    val_loader = DataLoader(
+        val_ds,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        pin_memory=pin_memory,
+        shuffle=False,
+    )
+
+    test_loader = DataLoader(
+        test_ds,
+        batch_size=1,
+        num_workers=0,
+        pin_memory=False,
+        shuffle=False,
+    )
+
+    return train_loader, val_loader, test_loader
+
+
 def get_loaders(
     train_dir,
     train_maskdir,
