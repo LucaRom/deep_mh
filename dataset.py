278ad9daa78fff75c84cbe062418824a599800e4
G3om <luca.romanini@outlook.com>
2022-08-05 19:10:37 -0400

    Modify estrie_rasterio dataset to include all sen2 and lidar training data

--- dataset.py
+++ dataset.py
@@ -422,59 +422,84 @@ class KenaukDataset_rasterio2(Dataset):
 # rasterio classe fait juste retourner le path vers l'image et le CRS et les Transforms
 # sont extraits en dehors du dataset
 class estrie_rasterio(Dataset):
-    def __init__(self, image_dir, mask_dir, mnt_dir, classif_mode, transform=None):
-        self.image_dir = image_dir
-        self.mask_dir = mask_dir
-        self.mnt_dir = mnt_dir
+    """ 
+        The folder containing 'sen2_ete' is filtered to respect the wanted files shape for training. If this is not the 
+        case, you need to change '/sen2_ete' from the instance variable 'self.images' called in the init phase of the 
+        class.
+
+    """
+    def __init__(self, train_dir, classif_mode, transform=None):
+        self.image_dir = train_dir
         self.classif_mode = classif_mode
-        self.transform = transform
-        self.all_img = [x for x in os.listdir(image_dir) if x.endswith(('.tif'))]
-
-        # Tri sur les images # TODO Optimiser parce que très lent et difficile à debugger
-                                # peut être faire en parrallèle? ou juste faire le tri avant
-                                # avec une utility
-        # Remove images in folder with wrong shape and no datas
-        wanted_shape = (512, 512, 12) # might be better to set as variable/parameter
-        self.images = []
-
-        for i in self.all_img :
-            img_path = os.path.join(image_dir, i)
-            test_img = np.array(tiff.imread(img_path), dtype=np.float32)
-            
-            if np.any(test_img == 0) or test_img.shape != wanted_shape:
-                pass
-            else:
-                self.images.append(i)
+        self.transform = transform       
+        self.images = [x for x in os.listdir(train_dir + "/sen2_ete") if x.endswith(('.tif'))]
 
     def __len__(self):
         return len(self.images)
 
-    def __getitem__(self, index):
-        img_path = os.path.join(self.image_dir, self.images[index])
-        mnt_path = os.path.join(self.mnt_dir, self.images[index].replace("sen2", "lidar_mnt"))
-        
-        if self.classif_mode == "bin":
-            mask_path = os.path.join(self.mask_dir, self.images[index].replace("sen2", "mask_bin"))
-        elif self.classif_mode == "multiclass":
-            mask_path = os.path.join(self.mask_dir, self.images[index].replace("sen2", "mask_multiclass"))
-        else:
-            print("There is somethigng wrong with your mask dataset or paths (dataset.py)")
-        
-        img_opt = np.array(tiff.imread(img_path), dtype=np.float32)
+    def __getitem__(self, index):      
+        # sentinel 2 images
+        sen2_ete_path = os.path.join(self.image_dir, 'sen2_ete', self.images[index])
+        sen2_print_path = os.path.join(self.image_dir, 'sen2_print', self.images[index].replace("ete", "print"))
 
+        # lidar images
+        mnt_path = os.path.join(self.image_dir, 'mnt', self.images[index].replace("sen2_ete", "mnt"))
+        mhc_path = os.path.join(self.image_dir, 'mhc', self.images[index].replace("sen2_ete", "mhc"))
+        slopes_path = os.path.join(self.image_dir, 'pentes', self.images[index].replace("sen2_ete", "pentes"))
+        tpi_path = os.path.join(self.image_dir, 'tpi', self.images[index].replace("sen2_ete", "tpi"))
+        tri_path = os.path.join(self.image_dir, 'tri', self.images[index].replace("sen2_ete", "tri"))
+        twi_path = os.path.join(self.image_dir, 'twi', self.images[index].replace("sen2_ete", "twi"))
+
+        #print("my ass")
+
+        if self.classif_mode == "bin":
+            mask_path = os.path.join(self.image_dir, 'mask_bin', self.images[index].replace("sen2_ete", "mask_bin"))
+        elif self.classif_mode == "multiclass":
+            mask_path = os.path.join(self.image_dir, 'mask_multiclass', self.images[index].replace("sen2_ete", "mask_multiclass"))
+        else:
+            print("There is something wrong with your mask dataset or paths (dataset.py)")
+        
         # normalize the bands
         # clip the value between [0 - 10000]
-        img_opt = np.where(img_opt < 0, 0, img_opt)  # clip value under 0
-        img_opt = np.where(img_opt > 10000, 10000, img_opt)  # clip value over 10 000
+        #TODO function or loop to normalize images instead or repeating
+        # sen2_ete normalization
+        sen2_ete_img = np.array(tiff.imread(sen2_ete_path), dtype=np.float32)
+        sen2_ete_img = np.where(sen2_ete_img < 0, 0, sen2_ete_img)  # clip value under 0
+        sen2_ete_img = np.where(sen2_ete_img > 10000, 10000, sen2_ete_img)  # clip value over 10 000
+        sen2_ete_img = sen2_ete_img/10000 # divide the array by 10000 so all the value are between [0-1]
 
-        # divide the array by 10000 so all the value are between [0-1]
-        img_opt = img_opt/10000
+        # sen2_print normalization
+        sen2_print_img = np.array(tiff.imread(sen2_print_path), dtype=np.float32)
+        sen2_print_img = np.where(sen2_print_img < 0, 0, sen2_print_img)  # clip value under 0
+        sen2_print_img = np.where(sen2_print_img > 10000, 10000, sen2_print_img)  # clip value over 10 000
+        sen2_print_img = sen2_print_img/10000 # divide the array by 10000 so all the value are between [0-1]
 
+        # stack both sentinel 2 images
+        img_opt = np.dstack((sen2_ete_img, sen2_print_img))
+
+        # Lidar images
+        # TODO loop / function to expand_dims
         img_mnt = np.array(tiff.imread(mnt_path))
         img_mnt = np.expand_dims(img_mnt, axis=2)
 
-        img_opt = np.dstack((img_opt, img_mnt))
+        img_mhc = np.array(tiff.imread(mhc_path))
+        img_mhc = np.expand_dims(img_mhc, axis=2)
 
+        img_slopes = np.array(tiff.imread(slopes_path))
+        img_slopes = np.expand_dims(img_slopes, axis=2)
+
+        img_tpi = np.array(tiff.imread(tpi_path))
+        img_tpi = np.expand_dims(img_tpi, axis=2)
+
+        img_tri = np.array(tiff.imread(tri_path))
+        img_tri = np.expand_dims(img_tri, axis=2)
+
+        img_twi = np.array(tiff.imread(twi_path))
+        img_twi = np.expand_dims(img_twi, axis=2)
+
+        img_lidar = np.dstack((img_mnt, img_mhc, img_slopes, img_tpi, img_tri, img_twi))
+
+        # Mask images
         #mask = np.array(tiff.imread(mask_path)) / 255
         mask = np.array(tiff.imread(mask_path)) 
         #mask[mask == 255.0] = 1.0
@@ -484,11 +509,11 @@ class estrie_rasterio(Dataset):
         # Cast to tensor for better permute
         img_opt = torch.from_numpy(img_opt)
         img_opt = img_opt.permute(2,0,1)
-        img_mnt = torch.from_numpy(img_mnt)
-        img_mnt = img_mnt.permute(2,0,1)
+        img_lidar = torch.from_numpy(img_lidar)
+        img_lidar = img_lidar.permute(2,0,1)
         mask  = torch.from_numpy(mask)
 
-        return img_opt, img_mnt, mask, img_path
+        return img_opt, img_lidar, mask, sen2_ete_path
 
 if __name__ == "__main__":
 # Import data with custom loader
@@ -501,20 +526,28 @@ if __name__ == "__main__":
 
 
     # Path Estrie
-    e_img_dir = "D:/00_Donnees/01_trainings/02_mh_double_stack/estrie/sen2"
+    e_img_dir = "/mnt/Data/00_Donnees/02_maitrise/01_trainings/estrie/512/"
     e_mask_dir = "D:/00_Donnees/01_trainings/02_mh_double_stack/estrie/mask_bin"
     #e_mask_dir = "D:/00_Donnees/01_trainings/02_mh_double_stack/estrie/mask_multi" # Multiclass
     e_lidar_dir = "D:/00_Donnees/01_trainings/02_mh_double_stack/estrie/lidar_mnt"
 
-    PIN_MEMORY = True
-    NUM_WORKERS = 1
-    BATCH_SIZE = 4
-    num_epochs = 4   
-    optim_main = "Ad"  # 'Ad' ou 'sg'
-    lr_main = 0.001
-    num_layers_main = 5
-    #print("ATTENTION NUM LAYERS AT 4 INSTEAD OF 5")
-    input_channel_main = 14
+    # Dataset Estrie
+    train_estrie_ds = estrie_rasterio(
+        train_dir=e_img_dir,
+        classif_mode= "multiclass",
+        #transform=train_transform
+    )
+
+    train_estrie_loader = DataLoader(
+        train_estrie_ds,
+        batch_size=4,
+        num_workers=6,
+        pin_memory=True,
+        shuffle=True,
+    )
+
+    batch = next(iter(train_estrie_loader))
+    print(batch)
 
     # train_ds = KenaukDataset(
     #     image_dir=TRAIN_IMG_DIR,
@@ -546,20 +579,13 @@ if __name__ == "__main__":
     #     shuffle=True,
     # )
 
-    # Dataset Estrie
-    train_estrie_ds = estrie_rasterio(
-        image_dir=e_img_dir,
-        mask_dir=e_mask_dir,
-        mnt_dir=e_lidar_dir,
-        #transform=train_transform
-    )
 
     # Random split
-    train_set_size = int(len(train_estrie_ds) * 0.7)
-    valid_set_size = len(train_estrie_ds) - train_set_size
-    train_set, valid_set = random_split(train_estrie_ds, [train_set_size, valid_set_size])
+    # train_set_size = int(len(train_estrie_ds) * 0.7)
+    # valid_set_size = len(train_estrie_ds) - train_set_size
+    # train_set, valid_set = random_split(train_estrie_ds, [train_set_size, valid_set_size])
 
-    print('debug')
+    # print('debug')
 
     # train_estrie_loader = DataLoader(
     #     train_estrie_ds,
