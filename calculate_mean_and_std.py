ab2d3582521860333f449bd55ae12e690ab53014
G3om <luca.romanini@outlook.com>
2022-11-13 15:26:48 -0500

    Add saving means and stdevs into files for later uses

--- calculate_mean_and_std.py
+++ calculate_mean_and_std.py
@@ -1,5 +1,9 @@
-import rasterio
 import numpy as np
+import os
+import rasterio
+import time
+
+import img_paths
 
 def mean_std_solo(path_image):
     dataset = rasterio.open(path_image)
@@ -8,6 +12,8 @@ def mean_std_solo(path_image):
     #count = 1
     mean_lst = []
     std_lst = []
+    num_bands = len(array)
+
     for band in array:
         band_flat = band.flatten()
         mean = np.average(band_flat)
@@ -15,10 +21,13 @@ def mean_std_solo(path_image):
         mean_lst.append(mean)
         std_lst.append(std)
 
+    print("Number of bands : ", num_bands)
     print("List of means : ")
     print(mean_lst)
     print("List of std: ")
     print(std_lst)
+
+    return mean_lst, std_lst, num_bands
 
 #mean_std_solo(k_image_ete)
 
@@ -59,34 +68,6 @@ def mean_std_pair(path_image1, path_image2):
     print(std_lst)
 
 # Calculated std and means for standardization
-# 12 bands estrie
-estrie_mean_e = [259.971087045696, 277.3490067676725, 520.4650232890134, 342.23574780553645, 906.7611488412249, 2656.3582951694643, 3203.3543093369944, 3389.6250611778078, 3487.079600166239, 3555.416409200909, 1714.2260907527316, 828.2768740555728]
-estrie_std_e  = [525.5551122108338, 526.4768589585602, 515.8903727938966, 527.3656790023017, 561.5222503677404, 836.1454714836563, 984.9190349745415, 1067.0420278801334, 1026.7569263359944, 1066.123618103052, 630.0584359871733, 505.2076063419134]
-estrie_mean_p = [457.4229830346009, 501.79759875320303, 694.4711397083421, 835.1158882308216, 1219.9447441650816, 1823.0661322180392, 2064.6505317461747, 2316.1887302003915, 2363.5869859139643, 2359.4662122932396, 2390.6124116260303, 1586.6126304451745]
-estrie_std_p  = [169.44646075504082, 249.03030944938908, 293.96819726121373, 408.20429488371605, 392.1811051266158, 492.36521601358254, 550.8773405439316, 623.9017038640061, 590.0457818993959, 540.556974947324, 740.4564895487368, 581.7629650224691]
-
-estrie_s1_mean_e = [-15.479797, -9.211855, 6.267961]
-estrie_s1_std_e  = [1.622046, 1.8651232, 1.2285297]
-estrie_s1_mean_p = [-15.0310545, -9.519093, 5.5120163]
-estrie_s1_std_p  = [2.1044014, 1.9065734, 1.37706]
-
-s1_e_p_mean = [-15.479797, -9.211855, 6.267961, -15.0310545, -9.519093, 5.5120163]
-s1_e_p_std  = [1.622046, 1.8651232, 1.2285297, 2.1044014, 1.9065734, 1.37706] 
-
-estrie_mhc_mean = [7.798849]
-estrie_mhc_std  = [7.033332]
-estrie_slope_mean = [5.5523205]
-estrie_slope_std  = [5.196636]
-estrie_tpi_mean = [0.0029951811]
-estrie_tpi_std  = [1.0641352]
-estrie_tri_mean = [0.06429929]
-estrie_tri_std  = [0.06102526]
-estrie_twi_mean = [6.7409873]
-estrie_twi_std  = [3.182435]
-
-estrie_lidar_mean = [7.798849, 5.5523205, 0.0029951811, 0.06429929, 6.7409873]
-estrie_lidar_std  = [7.033332, 5.196636, 1.0641352, 0.06102526, 3.182435]
-
 # 12 bands kenauk 2016
 kenauk_2016_mean_e = [396.8236398831345, 340.8023674183238, 564.6683101976018, 316.86383555478693, 899.6916374891973, 3054.401784107393, 3726.6279551992707, 3804.23744469375, 3982.4497302888194, 3999.5862881874696, 1907.939020030273, 842.7017527775387]
 kenauk_2016_std_e  = [34.77012868618949, 53.682757226651596, 101.81933949389413, 86.81071558339808, 223.97258987335917, 930.740342457327, 1192.9850511650027, 1264.8917375808307, 1295.2198436031365, 1193.0749293048152, 634.0964063702438, 290.9939493314744]
@@ -99,43 +80,41 @@ kenauk_estrie_std_e  = [449.1333965424019, 447.09781235125655, 440.1281124724915
 kenauk_estrie_mean_p = [389.73178500319295, 418.7855774087138, 656.2461824601817, 663.8277464836387, 1164.82661126222, 2267.199178786665, 2609.65239794843, 2826.5757717474403, 2885.967306830988, 2885.0676772511297, 2244.2198662295045, 1384.4035653061455]
 kenauk_estrie_std_p  = [182.1481184415827, 252.22961441363324, 271.82436188432376, 444.23478736031086, 384.49626301643093, 1043.6492154677742, 1255.1022957221837, 1267.5933253040087, 1263.0965697948964, 1229.1994225452902, 745.0200230290296, 607.8933293590522]
 
+k_lidar_means = [13.348262, 13.45669, -0.006740755, -3.689763, 5.7766604]
+k_lidar_stds  = [7.7406297, 13.942361, 1.3129127, 241.4134, 5.6496654]
 
 if __name__ == "__main__":
 
-    # Estrie sentinel 2 image : 
-    path_sen2_ete = "D:/00_Donnees/02_maitrise/01_trainings/estrie/processed_raw/sen2/ete/S2_estrie_3m_ete_septembre2020.tif"
-    path_sen2_prin = "D:/00_Donnees/02_maitrise/01_trainings/estrie/processed_raw/sen2/print/S2_estrie_3m_printemps_mai2020.tif"
+    paths_lst = img_paths.get_estrie_raw_paths(output_mode='list')
 
-    path_sen1_ete = "D:/00_Donnees/02_maitrise/01_trainings/estrie/processed_raw/sen1/ete/S1_estrie_3m_ete_septembre2020.tif"
-    path_sen1_prin = "D:/00_Donnees/02_maitrise/01_trainings/estrie/processed_raw/sen1/print/S1_estrie_3m_printemps_mai2020.tif"
+    lidar_means_array = []
+    lidar_stdev_array = []
+    for path in paths_lst:
+        mean_lst, std_lst, num_bands = mean_std_solo(path_image=path)
+        sensors_name = path.split("/")[-2]
+        current_region = path.split("/")[-4]
 
-    path_mhc = "D:/00_Donnees/02_maitrise/01_trainings/estrie/processed_raw/lidar/mhc_estrie_3m.tif"
-    path_slopes = "D:/00_Donnees/02_maitrise/01_trainings/estrie/processed_raw/lidar/pente_estrie_3m.tif"
-    path_tpi = "D:/00_Donnees/02_maitrise/01_trainings/estrie/processed_raw/lidar/tpi_estrie_3m.tif"
-    path_tri = "D:/00_Donnees/02_maitrise/01_trainings/estrie/processed_raw/lidar/tri_estrie_3m.tif"
-    path_twi  = "D:/00_Donnees/02_maitrise/01_trainings/estrie/processed_raw/lidar/twi_estrie_3m.tif"
+        if sensors_name == 'lidar':
+            # lidar_prefix = path.split('/')[-1][:3]
+            # output_name = os.path.join('stats/', current_region, current_region + "_" + sensors_name + "_" + lidar_prefix)
+            lidar_means_array.append(mean_lst[0])
+            lidar_stdev_array.append(std_lst[0])
+        else:
+            output_name = os.path.join('stats/', current_region, current_region + "_" + sensors_name)
 
+            print('Saving means for ', sensors_name)
+            np.save(output_name + '_means', mean_lst)
+            
+            print('Saving stdevs for ', sensors_name)
+            np.save(output_name + '_stds', std_lst)
 
-    # image_ete = "/mnt/Data/00_Donnees/02_maitrise/01_trainings/estrie/processed_raw/sen2/ete/S2_estrie_3m_ete_septembre2020.tif"
-    # image_prin = "/mnt/Data/00_Donnees/02_maitrise/01_trainings/estrie/processed_raw/sen2/print/S2_estrie_3m_printemps_mai2020.tif"
+    #current_region = path.split("/")[-4]
+    current_region = 'estrie'
+    lidar_output_name = os.path.join('stats/', current_region, current_region + "_lidar")
+    print('Saving means for LiDAR')
+    np.save(lidar_output_name + '_means', lidar_means_array )
+    
+    print('Saving stdevs for LIDAR')
+    np.save(lidar_output_name + '_stds', lidar_stdev_array)
 
-    # k_image_ete = "/mnt/Data/00_Donnees/02_maitrise/01_trainings/kenauk_2016/processed_raw/sen2/ete/s2_kenauk2016_3m_ete.tif"
-    # k_image_prin = "/mnt/Data/00_Donnees/02_maitrise/01_trainings/kenauk_2016/processed_raw/sen2/print/s2_kenauk2016_3m_print.tif"
-
-    #Kenauk
-    #k_sen2_ete = "D:/00_Donnees/02_maitrise/01_trainings/kenauk/processed_raw/sen2/ete/s2_kenauk_3m_ete_aout2020.tif"
-    #k_sen2_prin = "D:/00_Donnees/02_maitrise/01_trainings/kenauk/processed_raw/sen2/print/S2_de_kenauk_3m_printemps_mai2020.tif"
-
-    #k_sen1_ete = "D:/00_Donnees/02_maitrise/01_trainings/kenauk/processed_raw/sen1/ete/s1_kenauk_3m_ete_aout2020.tif"
-    #k_sen1_prin = "D:/00_Donnees/02_maitrise/01_trainings/kenauk/processed_raw/sen1/print/S1_kenauk_3m_printemps_mai2020.tif"
-
-    k_mhc = "D:/00_Donnees/02_maitrise/01_trainings/kenauk/processed_raw/lidar/mhc_kenauk_3m.tif"
-    k_slopes = "D:/00_Donnees/02_maitrise/01_trainings/kenauk/processed_raw/lidar/pentes_kenauk_3m.tif"
-    k_tpi = "D:/00_Donnees/02_maitrise/01_trainings/kenauk/processed_raw/lidar/tpi_kenauk_3m.tif"
-    k_tri = "D:/00_Donnees/02_maitrise/01_trainings/kenauk/processed_raw/lidar/tri_kenauk_3m.tif"
-    k_twi  = "D:/00_Donnees/02_maitrise/01_trainings/kenauk/processed_raw/lidar/twi_kenauk_3m.tif"
-
-    k_lidar_means = [13.348262, 13.45669, -0.006740755, -3.689763, 5.7766604]
-    k_lidar_stds  = [7.7406297, 13.942361, 1.3129127, 241.4134, 5.6496654]
-
-    mean_std_solo(k_twi)
+    #print(np.load('stats/estrie/estrie_lidar_means.npy'))
